77d3cf135004c07876b2e30e4928942baa4e65cf
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-06-13 15:03:01 -0700

    Added comments

--- ClassPractice.py
+++ ClassPractice.py
@@ -1,17 +1,17 @@
 
 
-class Vehicle:
+class Vehicle: #Creating a parent class
     engineType = 'Gasoline'
     physicalForm = 'Coupe'
     numberOfDoors = '2'
     accelerationMethod = 'Gas Pedal'
     decelerationMethod = 'Brake Pedal'
 
-class Car(Vehicle):
+class Car(Vehicle):#Creating a child class
     numberOfWheels = 4
     trunkSpace = '4 Cubic Feet'
 
-class Helicopter(Vehicle):
+class Helicopter(Vehicle):#Creating a child class
     numberOfRotors = 4
     maxLiftCapacity = '4000 lbs'
     
