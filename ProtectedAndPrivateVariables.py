3b07e1c793869fdfbeaf32cf5ea227804ee678bf
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-06-15 11:50:37 -0700

    Fixed ProtectedAndPrivate

--- ProtectedAndPrivateVariables.py
+++ ProtectedAndPrivateVariables.py
@@ -4,26 +4,17 @@
 # Here is an example of a protected variable, denoted by a single "_"
 class Protected:
     def __init__(self):
-        self._proVar = 100
+        self._proVar = 100 # This variable is protected, as denoted by "_"
+        self.__priVar = 25 # This variable is private, as denoted by "__"
 
-Object1 = Protected()
-print(Object1._proVar)
-Object1._proVar = 50
-print(Object1._proVar)
-
-
-# Here is an example of a private variable, denoted by two "_"
-class Private:
-    def __init__(self): # Initializes variable
-        self.__priVar = 25
-        
-    def getPrivate(self): # Prints current variable
+    def getPrivate(self):
         print(self.__priVar)
 
-    def setPrivate(self, variable): # Sets a new variable
+    def setPrivate(self, variable):
         self.__priVar = variable
 
-Object2 = Private()
-Object2.getPrivate()
-Object2.setPrivate(0)
-Object2.getPrivate()
+
+Object = Protected()
+print(Object._proVar)
+Object.getPrivate()
+
