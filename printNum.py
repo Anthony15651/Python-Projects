cc6fe8282877502ddd1e309a9ac65c0e94d3d041
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-12-26 13:07:12 -0800

    Testing code for Remotasks

--- printNum.py
+++ printNum.py
@@ -1,8 +1,26 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
 
-import ourModule
+    def make_sound(self):
+        raise NotImplementedError("Subclasses must implement the 'make_sound' method")
 
+class Dog(Animal):
+    def make_sound(self):
+        return "Woof!"
 
+class Cat(Animal):
+    def make_sound(self):
+        return "Meow!"
+
+class Bird(Animal):
+    def make_sound(self):
+        return "Chirp!"
+
+def animal_sounds(animals):
+    for animal in animals:
+        print(f"{animal.name} says {animal.make_sound()}")
 
 if __name__ == "__main__":
-    results = ourModule.getNumbers(5,9)
-    print(results)
+    animals = [Dog("Fido"), Cat("Whiskers"), Bird("Tweety")]
+    animal_sounds(animals)
